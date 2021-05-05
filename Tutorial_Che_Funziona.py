e5f993a36a138adb6e5ecbd013a54c5b8f410023
francipower13 <40489418+francipower13@users.noreply.github.com>
2021-05-05 17:38:48 +0200

    Update Tutorial_Che_Funziona.py

--- Tutorial_Che_Funziona.py
+++ Tutorial_Che_Funziona.py
@@ -41,11 +41,14 @@ class NeuralNetwork(nn.Module):
         super(NeuralNetwork, self).__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512), # strati, dati input - output
+            nn.Linear(1296*972, 512), # strati, dati input - output
+            
+            #TODO CONVOLUTIONAL
+            
             nn.ReLU(), # funzione sigmoide, rectified linear unit
             nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(512, 10),
+            nn.Linear(512, 1),
             nn.ReLU()
         )
 
@@ -86,19 +89,21 @@ def train(dataloader, model, loss_fn, optimizer):
 def test(dataloader, model):
     size = len(dataloader.dataset)
     model.eval()
-    test_loss, correct = 0, 0
+    test_loss = 0
+    #test_loss, correct = 0, 0
     with torch.no_grad(): # azzera il gradiente del blocco di codice, riduce i consumi di memoria
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
+            
             # prendi i nodo piu acceso, se è uguale a y allora aggiungi 1 al numero di corretti.
-            print("pred arg max:",pred.argmax(1))
-            print("y:",y )
-            print("DEBUG:",pred.argmax(1) == y)
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            #print("pred arg max:",pred.argmax(1))
+            #print("y:",y )
+            #print("DEBUG:",pred.argmax(1) == y)
+            #correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= size
-    correct /= size
+    #correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
@@ -115,7 +120,7 @@ print("Done!")
 
 
 
-classes = [
+"""classes = [
     "T-shirt/top",
     "Trouser",
     "Pullover",
@@ -134,3 +139,4 @@ with torch.no_grad():
     pred = model(x)
     predicted, actual = classes[pred[0].argmax(0)], classes[y]
     print(f'Predicted: "{predicted}", Actual: "{actual}"')
+"""
