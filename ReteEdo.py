6f2f344d89f66ae4d9647b2ff57b5b6451027bae
Luca <43042873+LucaPalumbo@users.noreply.github.com>
2021-06-27 12:09:26 +0200

    Update ReteEdo.py

--- ReteEdo.py
+++ ReteEdo.py
@@ -17,7 +17,7 @@ import sys
 # per farlo funzionare prima dovete installare tensorboard, scrivere poi sul prompt: "tensorboard --logdir=runs", successivamente dovrebbe dirvi che ha creato un localhost a una certa porta, la mia è 6006. Accedete col browser e dovrebbe mostrarvi
 # tensorboard. A quel punto fate partire il programma
 from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter("runs/Project4")
+writer = SummaryWriter("runs/Project5")
 
 # Define the dataset class
 class Astropi(Dataset):
@@ -53,14 +53,15 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Parameters for the NN
 num_epochs = 10
-batch_size = 7
+batch_size = 5
 learning_rate = 0.0005
 
 # Load the dataset and split it between a train and a test set
-train_set, test_set = torch.utils.data.random_split(dataset, [700, 18])
+train_set, test_set = torch.utils.data.random_split(dataset, [568, 150])
 train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=True)
 
+"""
 examples = iter(test_loader)
 example_data, example_targets = examples.next()
 
@@ -78,6 +79,7 @@ print(img_grid.shape)
 writer.add_image('ISS_images',img_grid)
 #writer.close()
 #sys.exit()
+"""
 
 # Defines the network structure
 class ConvNet(nn.Module):
@@ -87,9 +89,9 @@ class ConvNet(nn.Module):
         # pool allows us to reduce the image size by only considering the maximum valued pixel in an area
         # a convolutional layer passes information of a pixel by considering the neighboring ones
         # a linear layer outputs a linear combination of the input layers to each output layer
-        self.conv1 = nn.Conv2d(3, 6, 5) 
-        self.pool1 = nn.MaxPool2d(3, 3) 
-        self.pool2 = nn.MaxPool2d(2, 2) 
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(3, 3)
+        self.pool2 = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.conv3 = nn.Conv2d(16, 20, 5)
         self.conv4 = nn.Conv2d(20, 33, 5)
@@ -114,7 +116,18 @@ model = ConvNet().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) # Stavolta usa SGD, non so perché non ADAM
 criterion = nn.MSELoss()
 
-writer.add_graph(model, example_data)
+def evaluates():
+    # Evaluates the network on the test set. We decided to output the loss because
+    # it can be easily calculated when comparing floating point values
+    with torch.no_grad():
+        cont = 0
+        loss = 0
+        for i, (images, labels) in enumerate(test_loader):
+            loss += criterion(model(images), labels.to(torch.float32).view(-1, 1))
+            cont +=1
+        print(f'Mean Squared Error {loss/cont:.8f}')
+
+#writer.add_graph(model, example_data)
 #writer.close()
 #sys.exit()
 n_total_steps = len(train_loader)
@@ -122,6 +135,7 @@ n_total_steps = len(train_loader)
 running_loss = 0.0
 print("funziona")
 for epoch in range(num_epochs):
+    evaluates()
     for i, (images, labels) in enumerate(train_loader):
         images = images.to(device)
         labels = labels.to(device)
@@ -136,18 +150,8 @@ for epoch in range(num_epochs):
              print(f'Epoch {epoch+1}/{num_epochs}, Step [{i+1}/{len(train_loader)}], Loss: {loss.item():.4f}')
              writer.add_scalar('training loss', running_loss / 100, epoch * n_total_steps + i)
              running_loss = 0
-
 print('Finished Training')
+evaluates()
 
 # Save the trained model for future use
 torch.save(model.state_dict(), "model")
-
-# Evaluates the network on the test set. We decided to output the loss because 
-# it can be easily calculated when comparing floating point values
-with torch.no_grad():
-    cont = 0
-    loss = 0
-    for i, (images, labels) in enumerate(test_loader):
-        loss += criterion(model(images), labels.to(torch.float32).view(-1, 1))
-        cont +=1
-    print(f'Mean Squared Error {loss/cont:.8f}')
